c00a027ebe863f41f2635efa55b851143bef4f58
James Taylor <user234683@users.noreply.github.com>
2019-01-26 17:00:18 -0800

    simple python server for testing

--- join.py
+++ join.py
@@ -2,8 +2,11 @@ import urllib
 import re
 from email.utils import parseaddr
 import os
+from string import Template
+import html
+import time
 
-members_csv = os.path.normpath('/data/members.csv')
+members_csv = os.path.normpath('./data/members.csv')
 members_directory = os.path.dirname(members_csv)
 # just completely remove commas, newlines, and quotes from field instead of dealing with stupid csv escaping
 # we don't need those characters in data anyway
@@ -12,7 +15,8 @@ def sanitize_field(field):
         field = field.replace(character, '')
     return field
 
-def add_member(email, student_id, first_name, last_name):
+
+def add_member(email, student_id, first_name, last_name): #, time_added, confirmation_code, confirmed):
     line_to_add = ','.join(sanitize_field(field) for field in (email, student_id, first_name, last_name))
 
     # check that directory with members.csv exists
@@ -23,19 +27,22 @@ def add_member(email, student_id, first_name, last_name):
             members = f.read()
     except FileNotFoundError:
         with open(members_csv, 'a+', encoding='utf-8') as f:
-            f.write('email,student_id,first_name,last_name\n' + line_to_add + '\n')
+            f.write('email,student_id,first_name,last_name,time_added,confirmation_code,confirmed\n' + line_to_add + '\n')
     else:
         start_position = members.find('\n' + email)
         if start_position == -1:
             members += line_to_add + '\n'
         else:   # member already exists, update line
-            end_position = members.find('\n', start=start_position+1)
+            end_position = members.find('\n', start_position+1)
             members = members[0:start_position+1] + line_to_add + members[end_position:]
         with open(members_csv, 'w', encoding='utf-8') as f:
             f.write(members)
 
+with open('./template.html', 'r', encoding='utf-8') as f:
+    basic_template = Template(f.read())
 def error_page(environ, start_response, message, code='400 Bad Request'):
-    pass
+    start_response(code, [('Content-Type', 'text/html')])
+    yield basic_template.substitute(title='Error', main='Error: ' + message).encode('utf-8')
 
 
 
@@ -44,41 +51,38 @@ def error_page(environ, start_response, message, code='400 Bad Request'):
 required_params = {'email', 'student_id', 'first_name', 'last_name'}
 
 def application(environ, start_response):
-    try:
-        method = environ['REQUEST_METHOD']
-        if method != "POST":
-            yield from error_page(environ, start_response, "Bad request", code="405 Method Not Allowed")
-            return
+    method = environ['REQUEST_METHOD']
+    if method != "POST":
+        yield from error_page(environ, start_response, "Bad request", code="405 Method Not Allowed")
+        return
 
-        # Read POST data
-        parameters = urllib.parse.parse_qs(environ['wsgi.input'].read().decode())
+    # Read POST data
+    parameters = urllib.parse.parse_qs(environ['wsgi.input'].read().decode())
 
-        # Check that all the required information is present
-        if not required_params.issubset(parameters.keys()):
-            message = "Bad request, missing parameters: " + ','.join(required_params - parameters.keys())
-            yield from error_page(environ, start_response, message)
-            return
+    # Check that all the required information is present
+    if not required_params.issubset(parameters.keys()):
+        message = "Bad request, missing parameters: " + ','.join(required_params - parameters.keys())
+        yield from error_page(environ, start_response, message)
+        return
 
-        email = parameters['email']
-        student_id = parameters['student_id']
-        first_name = parameters['first_name']
-        last_name = parameters['last_name']
+    email = parameters['email'][0]
+    student_id = parameters['student_id'][0]
+    first_name = parameters['first_name'][0]
+    last_name = parameters['last_name'][0]
 
 
-        # Check that 900 number is valid
-        if not re.fullmatch(r'900\d{6}', student_id):
-            yield from error_page(environ, start_response, 'Invalid student_id')
-            return
+    # Check that 900 number is valid
+    if not re.fullmatch(r'900\d{6}', student_id):
+        yield from error_page(environ, start_response, 'Invalid student_id')
+        return
 
-        # Check that email is valid
-        # https://stackoverflow.com/a/14485817/8146866
-        name, address = parseaddr(email)
-        if address == '' or not address.endswith('@my.vcccd.edu'):
-            yield from error_page(environ, start_response, 'Invalid email address')
-            return
+    # Check that email is valid
+    # https://stackoverflow.com/a/14485817/8146866
+    name, address = parseaddr(email)
+    if address == '' or not address.endswith('@my.vcccd.edu'):
+        yield from error_page(environ, start_response, 'Invalid email address')
+        return
 
-        add_member(email, student_id, first_name, last_name)
-
-    except Exception as e:
-        start_response('500 Internal Server Error', [('Content-Type', 'text/plain')] )
-        yield '500 Internal Server Error'
+    add_member(email, student_id, first_name, last_name)
+    start_response('200 OK', [('Content-Type', 'text/html')] )
+    yield basic_template.substitute(title='Success', main='An email has been sent to ' + html.escape(email) + ' to confirm your membership').encode('utf-8')
---
+++ sever.py
@@ -0,0 +1,41 @@
+from gevent import monkey
+monkey.patch_all()
+
+from gevent.pywsgi import WSGIServer
+import join
+import mimetypes
+
+static_files = {'/about.html', '/contact.html', '/faq.html', '/index.html', '/join.html'}
+def serve_file(environ, start_response):
+    if environ['REQUEST_METHOD'] != "GET":
+        yield from join.error_page(environ, start_response, "Bad request", code="405 Method Not Allowed")
+        return
+    try:
+        with open('.' + environ['PATH_INFO'], 'rb') as f:
+            data = f.read()
+    except FileNotFoundError:
+        start_response('404 Not Found', [('Content-Type', 'text/plain')])
+        yield b'404 Not Found'
+    else:
+        start_response('200 OK', [('Content-Type', mimetypes.guess_type(environ['PATH_INFO'])[0] )])
+        yield data
+
+def application(environ, start_response):
+    try:
+        path = environ['PATH_INFO']
+        if path == '/join':
+            yield from join.application(environ, start_response)
+        elif path in static_files or path.startswith('/stylesheets/') or path.startswith('/images/'):
+            yield from serve_file(environ, start_response)
+        else:
+            start_response('404 Not Found', [('Content-Type', 'text/plain')])
+            yield b'404 Not Found'
+
+    except Exception as e:
+        start_response('500 Internal Server Error', [('Content-Type', 'text/plain')] )
+        yield b'500 Internal Server Error'
+        raise
+
+server = WSGIServer(('127.0.0.1', 8080), application)
+print('Started httpserver on port ' , 8080)
+server.serve_forever()
