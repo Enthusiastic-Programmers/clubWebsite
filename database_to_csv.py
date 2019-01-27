888b082f054b3f9cab0a65c11fbe721d299a63c4
James Taylor <user234683@users.noreply.github.com>
2019-01-26 20:17:33 -0800

    database_to_csv: close database when done

--- database_to_csv.py
+++ database_to_csv.py
@@ -13,12 +13,15 @@ csv_path = os.path.normpath('./data/members.csv')
 database_path = os.path.normpath('./data/members.sqlite')
 
 connection = sqlite3.connect(database_path)
-cursor = connection.cursor()
-rows = cursor.execute("SELECT * FROM members")
+try:
+    cursor = connection.cursor()
+    rows = cursor.execute("SELECT * FROM members")
 
-members_csv = 'email,student_id,first_name,last_name,time_added,confirmation_token,confirmed\n'
-for row in rows:
-    members_csv += ','.join(sanitize_field(str(field)) for field in row) + '\n'
+    members_csv = 'email,student_id,first_name,last_name,time_added,confirmation_token,confirmed\n'
+    for row in rows:
+        members_csv += ','.join(sanitize_field(str(field)) for field in row) + '\n'
+finally:
+    connection.close()
 
 with open(csv_path, 'w', encoding='utf-8') as f:
     f.write(members_csv)
