5920cbe818f9ea002cbc0621423fc63c04335065
rogerfachini <r3fachini@live.com>
2019-02-11 15:49:28 -0800

    More docstrings and comments

--- clubWebsite/database/__init__.py
+++ clubWebsite/database/__init__.py
@@ -2,5 +2,5 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
-db = SQLAlchemy()
-migrate = Migrate()
+db = SQLAlchemy()   #: flask-SQLAlchemy instance. db.session can be used to access the current session
+migrate = Migrate() #: Alembic database migration integration
