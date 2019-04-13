09307f9f668898ef20584056a1ccc6df69f7792b
James Taylor <user234683@users.noreply.github.com>
2019-04-12 23:52:31 -0700

    Restructure database and join system for security and so corrections can be made

--- clubWebsite/database/models.py
+++ clubWebsite/database/models.py
@@ -2,32 +2,31 @@
 import datetime
 import secrets
 import os
-import traceback
-from sendgrid import SendGridAPIClient
-from sendgrid.helpers.mail import Mail
-from flask import url_for
-import flask
 
 from clubWebsite.database import db
-from clubWebsite.config import BaseConfig
 
 class Member(db.Model):
     """Member of the club who has signed up"""
     __tablename__ = 'members'
-    student_id = db.Column(db.Integer, nullable=False, index=True, primary_key=True) #: Student's ID number (900xxxxxxx)
+    member_id = db.Column(db.Integer, nullable=False, index=True, primary_key=True)  #: Internal SQL id
+    student_id = db.Column(db.Integer, nullable=False, index=True)                   #: Student's ID number (900xxxxxxx)
     email = db.Column(db.String(128), nullable=False, index=True)                    #: Student's e-mail address (@my.vcccd.edu)
     first_name = db.Column(db.String(64), nullable=False)                            #: Student's first name
     last_name = db.Column(db.String(64), nullable=False)                             #: Student's last name
     time_added = db.Column(db.DateTime(), default=datetime.datetime.utcnow)          #: Datetime (in UTC) when the registration occurred
-    confirmation_token = db.Column(db.String(64))                                    #: Generated confirmation token
+    confirmation_token = db.Column(db.String(64), index=True, unique=True)           #: Generated confirmation token
     confirmation_time = db.Column(db.DateTime())                                     #: When the confirmation token was sent (used to calculate token expiry)
     is_confirmed = db.Column(db.Boolean(), default=False)                            #: Boolean flag, is True once the student has confirmed their membership
 
-    def __init__(self, student_id, email, first_name, last_name, **kwargs):
+    def __init__(self, student_id, email, first_name, last_name, confirmation_token=None, confirmation_time=None, **kwargs):
         self.email = email
         self.student_id = student_id
         self.first_name = first_name
         self.last_name = last_name
+        if confirmation_token is not None:
+            self.confirmation_token = confirmation_token
+        if confirmation_time is not None:
+            self.confirmation_time = confirmation_time
         super(Member, self).__init__(**kwargs)
 
     def __repr__(self):
@@ -35,40 +34,13 @@ class Member(db.Model):
         return "<Member {}, {}, {}>".format(self.student_id, self.first_name, status)
 
     @staticmethod
-    def get_or_create(student_id, email, first_name, last_name, **kwargs):
-        """
-        Get the given member (by student ID) if it exists in the database,
-        otherwise create a new Member with the given parameters
-        """
-        current_member = Member.query.get(student_id)
-        if current_member:
-            return current_member
-        else:
-            new_member = Member(student_id, email, first_name, last_name, **kwargs)
-            db.session.add(new_member)
-            db.session.commit()
+    def create(student_id, email, first_name, last_name, **kwargs):
+        """Create a new Member with the given parameters"""
+        new_member = Member(student_id, email, first_name, last_name, **kwargs)
+        db.session.add(new_member)
+        db.session.commit()
 
-            confirmation_token = new_member.generate_confirmation_token()
-            confirmation_link = flask.request.url_root.rstrip('/') + url_for('views.confirm', id=student_id, confirmation_token=confirmation_token)
-            message = Mail(
-                from_email='no-reply@epclub.pythonanywhere.com',
-                to_emails=email,
-                subject='Club membership confirmation',
-                html_content='Click this link to confirm your membership: <a href="' + confirmation_link + '">' + confirmation_link + '</a>'
-            )
-
-            try:
-                key = BaseConfig.SENDGRID_API_KEY
-                if key is None:
-                    print("Confirmation email for %s failed. Missing SENDGRID_API_KEY." % first_name)
-                    return None
-                sg = SendGridAPIClient(key)
-                response = sg.send(message)
-            except Exception as e:
-                traceback.print_exc()
-                return None
-
-            return new_member
+        return new_member
 
     @staticmethod
     def prune_expired(expire_delta=48):
@@ -80,14 +52,14 @@ class Member(db.Model):
         Member.query.delete(delete_ids)
         db.session.commit()
 
-    def generate_confirmation_token(self):
+    @staticmethod
+    def generate_confirmation_token():
         """Generates a confirmation token"""
-        self.confirmation_token = secrets.token_urlsafe(nbytes=32)
-        self.confirmation_time = datetime.datetime.utcnow()
-        db.session.commit()
-        return self.confirmation_token
+        confirmation_token = secrets.token_urlsafe(nbytes=32)
+        confirmation_time = datetime.datetime.utcnow()
+        return confirmation_token, confirmation_time
 
-    def confirm(self, confirmation_token, expire_delta=48):
+    def confirm(self, expire_delta=48):
         """
         Confirms the membership with the given token. Defaults to 48h expiry period
         Returns a boolean with success/failure, and a string with verbose output
@@ -96,21 +68,20 @@ class Member(db.Model):
             # Already confirmed
             return True, "Your membership has already been confirmed"
 
-        elif not self.confirmation_token:
-            # Token not yet generated
-            return False, "Your confirmation email has not been sent out, please wait"
-
         elif self.has_token_expired(expire_delta):
             # Token expired
             return False, "Confirmation link expired, please try again"
 
-        elif self.confirmation_token == confirmation_token:
-            # Token is valid
+        else:
             self.is_confirmed = True
+
+            # Remove any other entries with this email address - they are either fake or outdated
+            for member in Member.query.filter(Member.email == self.email, Member.member_id != self.member_id).all():
+                db.session.delete(member)
+
             db.session.commit()
             return True, "Your membership has been confirmed"
-        else:
-            return False, "Confirmation token invalid, please try again"
+
 
     def get_token_expire_time(self, expire_delta):
         """Get the time at which the confirmation token will expire """
--- clubWebsite/routes.py
+++ clubWebsite/routes.py
@@ -4,10 +4,15 @@ import html
 import urllib
 from werkzeug import unescape
 
+import traceback
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail
+
 from flask import Blueprint, render_template, send_from_directory, request, redirect, url_for, abort, Response, flash
 
 from clubWebsite.forms import RegisterForm
 from clubWebsite.database.models import Member
+from clubWebsite.config import BaseConfig
 
 views_blueprint = Blueprint('views', __name__, template_folder='templates')
 
@@ -37,11 +42,32 @@ def join():
 
     # On form submission with valid input
     if request.method == 'POST' and valid:
-        #print(form.email.data, form.student_id.data, form.first_name.data, form.last_name.data, time_added, confirmation_token, False)
-        member = Member.get_or_create(form.student_id.data, form.email.data, form.first_name.data, form.last_name.data)
-        if member is None:
+        confirmation_token, confirmation_time = Member.generate_confirmation_token()
+        confirmation_link = request.url_root.rstrip('/') + url_for('views.confirm', confirmation_token=confirmation_token)
+        message = Mail(
+            from_email='no-reply@epclub.pythonanywhere.com',
+            to_emails=form.email.data,
+            subject='Club membership confirmation',
+            html_content='Click this link to confirm your membership: <a href="' + confirmation_link + '">' + confirmation_link + '</a>'
+        )
+
+        try:
+            key = BaseConfig.SENDGRID_API_KEY
+            if key is None:
+                print("Confirmation email for %s failed. Missing SENDGRID_API_KEY." % form.first_name.data)
+                abort(500)
+                abort(Response('Failed to send confirmation email. Please contact the website administration.'))
+            sg = SendGridAPIClient(key)
+            response = sg.send(message)
+        except Exception as e:
+            traceback.print_exc()
             abort(500)
-            abort(Response('Failed to send confirmation email. Please try again.'))
+            abort(Response('Failed to send confirmation email. Please try again later.'))
+
+
+
+        Member.create(form.student_id.data, form.email.data, form.first_name.data, form.last_name.data, 
+                      confirmation_token=confirmation_token, confirmation_time=confirmation_time)
 
         # Redirect to email_sent page
         return redirect(url_for('views.email_sent', email=form.email.data))
@@ -56,25 +82,23 @@ def email_sent():
         abort(400)
         abort(Response('Missing email'))
     msg = 'An email has been sent to %s to confirm your membership' % email
-    # TODO: Send confirmation email here
     
     flash(msg)
     return render_template("base.html", title="Registered")
 
 @views_blueprint.route("/confirm")
 def confirm():
-    student_id = request.args.get('id')
     token = request.args.get('confirmation_token')
-    if not token or not student_id:
+    if not token:
         abort(400)
-        abort(Response('Missing student id or confirmation token'))
+        abort(Response('Missing confirmation token'))
 
-    member = Member.query.get(int(student_id))
+    member = Member.query.filter_by(confirmation_token=token).one_or_none()
     if not member:
-        abort(400)
-        abort(Response("Invalid student id"))
+        flash("Invalid or expired confirmation link. Please try again.")
+        return render_template("base.html", title="Membership confirmation")
 
-    status, message = member.confirm(token)
+    status, message = member.confirm()
 
     flash(message)
     return render_template("base.html", title="Membership confirmation")
