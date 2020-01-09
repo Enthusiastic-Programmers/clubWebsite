ca2ca25889a50bedc99ac90f8581e1190aa30355
James Taylor <user234683@users.noreply.github.com>
2020-01-08 16:19:44 -0800

    Fix transparent navbar, center hamburger menu vertically in navbar

--- clubWebsite/static/js/hackathon.js
+++ clubWebsite/static/js/hackathon.js
@@ -18,7 +18,7 @@ $(document).ready(function() {
             'id': ''
         });
         $('body').append($mobile_nav);
-        $('body').prepend('<button type="button" id="mobile-nav-toggle"><i class="fa fa-bars"></i></button>');
+        $('header').append('<button type="button" id="mobile-nav-toggle"><i class="fa fa-bars"></i></button>');
         $('body').append('<div id="mobile-body-overly"></div>');
         $('#mobile-nav').find('.menu-has-children').prepend('<i class="fa fa-chevron-down"></i>');
 
